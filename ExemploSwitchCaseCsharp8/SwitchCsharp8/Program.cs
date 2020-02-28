761a180f32d0fafbf8dab527d36701d9ef4a0f84
lucaslima10077 <lucaslima10077@gmail.com>
2020-02-28 10:11:11 -0300

    Add project files.

---
+++ ExemploSwitchCaseCsharp8/SwitchCsharp8/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ExemploSwitchCaseCsharp8
+{
+    class Program
+    {
+        static void Main()
+        {
+            var resultado = GetOperador(Operador.somar);
+
+            Console.WriteLine(resultado);
+
+            SwitchExemplo();
+        }
+
+        public enum Operador
+        {
+            subtrair,
+            multiplicar,
+            somar
+        }
+
+        private static string GetOperador(Operador operador) => operador switch
+        {
+            Operador.multiplicar => "Multiplicar",
+            Operador.somar => "Somar",
+            Operador.subtrair => "subtrair",
+            _ => throw new ArgumentException("Operador desconhecido")
+        };
+
+
+        private static void SwitchExemplo()
+        {
+            var operador = "+";
+            var resultado = operador switch
+            {
+                "-" => "Subtrair",
+                "*" => "Multiplicar",
+                "+" => "Somar",
+                "_" => "Operador desconhecido"
+            };
+
+            Console.WriteLine(resultado);
+        }
+    }
+}
